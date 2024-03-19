b38085ed1fe15a253ef93f8840c0f1e6525bd116
Altegar <91095463+Altegar@users.noreply.github.com>
2024-03-19 22:54:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Сушинський Ігор
+// Лабораторна робота № 6
+// Літерні рядки.
+// Варіант 3
+
+namespace LAB_6_CSharp
+{
+    internal class Program
+    {
+        private static int Count(string s)
+        {
+            int count = 0;
+            foreach (char c in s)
+            {
+                if (c == ',')
+                    count++;
+            }
+            return count;
+        }
+
+        private static string Change(string s)
+        {
+            string t = s.Replace(",", "**");
+            return t;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("s = ");
+            string s = Console.ReadLine();
+            Console.WriteLine($"',' includes {Count(s)} times");
+            Console.WriteLine($"new string = {Change(s)}");
+        }
+    }
+}
